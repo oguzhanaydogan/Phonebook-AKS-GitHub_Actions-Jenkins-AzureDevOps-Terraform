da2aeaa973ada1e09b928fef7899805f8faf7378
Oguzhan AYDOGAN <oguzhan.aydogan@deop.ca>
2023-04-14 13:49:12 -0400

    variables added

--- Terraform_Infra_AzureDevops/variables.tf
+++ Terraform_Infra_AzureDevops/variables.tf
@@ -13,12 +13,12 @@ variable "environment" {
 variable "personal_access_token" {
   description = "Azure DevOps personal access token"
   type        = string
-  default     = "${var.AZDO_PERSONAL_ACCESS_TOKEN}"
+  default     = ""
 }
 
 variable "organization_url" {
   description = "Azure DevOps personal organization_url"
   type        = string
-  default     = "${var.AZDO_ORG_SERVICE_URL}"
+  default     = ""
 }
 
